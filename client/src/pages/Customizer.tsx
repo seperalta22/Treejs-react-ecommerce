cdb3250c80a4f4cb73b6d707ba5a8d6a1ecc5ea5
Sergio Peralta <seperalta88@gmail.com>
2023-05-06 14:34:44 -0300

    Update homepage 1

--- client/src/pages/Customizer.tsx
+++ client/src/pages/Customizer.tsx
@@ -1,4 +1,15 @@
 const Customizer = () => {
-	return <div>Customizer</div>;
+	return (
+		<div>
+			Customizer
+			{/* <button className='download-btn' onClick={downloadCanvasToImage}>
+              <img
+                src={download}
+                alt='download_image'
+                className='w-3/5 h-3/5 object-contain'
+              />
+            </button> */}
+		</div>
+	);
 };
 export default Customizer;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,48 @@
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+import {
+	headContainerAnimation,
+	headContentAnimation,
+	headTextAnimation,
+	slideAnimation,
+} from '../config/motion';
+
 const Home = () => {
-	return <div>Home</div>;
+	const snap = useSnapshot(state);
+	return (
+		<AnimatePresence>
+			{snap.intro && (
+				<motion.div className='home' {...slideAnimation('left')}>
+					<motion.header {...slideAnimation('down')}>
+						<img
+							src='./threejs.png'
+							alt='threejs'
+							className='w-8 h-8 object-contain'
+						/>
+					</motion.header>
+					<motion.div className='home-content' {...headContainerAnimation}>
+						<motion.div {...headTextAnimation}>
+							<h1 className='head-text'>
+								LET'S <br className='xl:block hidden' />
+								<span className='text-blue-500'>DO IT.</span>
+							</h1>
+						</motion.div>
+						<motion.div
+							{...headContentAnimation}
+							className='flex flex-col gap-5'
+						>
+							<p className='max-w-md font-normal text-gray-600 text-base'>
+								Create your unique and exclsive shirt whit our brand-new 3D
+								customization tool. <strong>Unleash your imagination</strong>{' '}
+								and create your own shirt.
+							</p>
+						</motion.div>
+					</motion.div>
+				</motion.div>
+			)}
+		</AnimatePresence>
+	);
 };
 export default Home;
