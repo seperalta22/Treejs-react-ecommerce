6c2da4c76afd4b38c7890d1256397b623e5edbcf
Sergio Peralta <seperalta88@gmail.com>
2023-05-07 16:56:31 -0300

    finish first version of backdrop

--- client/src/canvas/index.tsx
+++ client/src/canvas/index.tsx
@@ -6,11 +6,16 @@ import Shirt from './Shirt';
 
 const CanvasModel = () => {
 	return (
-		<Canvas>
+		<Canvas
+			shadows
+			camera={{ position: [0, 0, 0], fov: 25 }}
+			gl={{ preserveDrawingBuffer: true }}
+			className='w-full max-w-full h-full transition-all ease-in'
+		>
 			<ambientLight intensity={0.5} /> // soft white light
 			<Environment preset='city' /> // city, forest, night, warehouse
 			<CameraRig>
-				{/* <Backdrop /> */}
+				<Backdrop />
 				<Center>
 					<Shirt />
 				</Center>
